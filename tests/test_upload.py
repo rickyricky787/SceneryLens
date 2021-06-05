ee0e0995cc7f2d9f860a2a8b888ef592c37c0120
rickyricky787 <rrodriguez2017@gmail.com>
2021-06-05 12:18:36 -0400

    Added new test for url image

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -7,7 +7,7 @@ import os
 from flask_app.app import app
 
 # This set of tests checks for uploading images
-class testValidation(unittest.TestCase):
+class testUpload(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
     
@@ -19,9 +19,20 @@ class testValidation(unittest.TestCase):
             with open('tests/test.jpg', 'rb') as img1:
                 imgBytesIO = io.BytesIO(img1.read())
 
-            data = {}
+            data = {'img_link': ""}
             data['file'] = (imgBytesIO, 'test.jpg')
             
+            r = self.app.post("/", data = data, follow_redirects=True)
+            if r.status_code != 200:
+                self.fail("Page did not render results page.")
+    
+    def test_img_url(self):
+        with self.app as c:
+            data = {
+                "img_link": "https://picsum.photos/id/1043/5184/3456",
+                "file": ""
+            }      
+
             r = self.app.post("/", data = data, follow_redirects=True)
             if r.status_code != 200:
                 self.fail("Page did not render results page.")
